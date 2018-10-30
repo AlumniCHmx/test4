c74ea6990b96eb9e8d24bd428da202bb86f4f8d8
JesusMoralesF <emorales41@gmail.com>
2018-10-30 13:49:23 -0600

    nav selector

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ var maxAlt=altura-20;
 document.getElementById("content").style.height=`${maxAlt}px`
 document.getElementById('doc').setAttribute("height",maxAlt)
 
-PDFObject.embed("CBImath.pdf", "#example1");
+var cajas=document.querySelectorAll('.blurred-box');
+
+cajas.forEach(function(element) {
+    //console.log(element.getAttribute('data-tipo'));
+    element.setAttribute('onclick','print(this)')
+});
+
+function print(e){
+    document.getElementsByTagName('h1')[0].style.display='none'
+    document.getElementById("navSection").style.display='none'
+    var tipo =e.getAttribute('data-tipo')
+    
+    document.getElementById("content").style.display='block'
+    var exam= `pdf/Examen UAM ${tipo}.pdf`
+    PDFObject.embed(exam, "#example1");
+
+}
+
